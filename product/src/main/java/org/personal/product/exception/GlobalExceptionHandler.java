486739a08411dd10391d7ddbd171a254eb9b8d53
Cristea Bogdan <cristea.ibogdan@gmail.com>
2024-06-01 18:38:50 +0300

    Switched from ControllerAdvice to RestControllerAdvice to reduce boilerplate code

--- product/src/main/java/org/personal/product/exception/GlobalExceptionHandler.java
+++ product/src/main/java/org/personal/product/exception/GlobalExceptionHandler.java
@@ -3,26 +3,25 @@ package org.personal.product.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @Value("${spring.application.name}")
     private String microserviceName;
 
     @ExceptionHandler(CustomException.class)
-    ResponseEntity<Object> handleCustomException(CustomException e) {
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    ErrorResponse handleCustomException(CustomException e) {
         log.error("Exception thrown in microservice: {} with the message: {}",
                 microserviceName,
                 e.getMessage());
 
         //TODO: Maybe i should remove ErrorResponse since the frontend only needs a code.
-        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
-
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorResponse);
+        return new ErrorResponse(e.getErrorCode());
     }
 }
--- washing-machine/src/main/java/org/personal/washingmachine/exception/GlobalExceptionHandler.java
+++ washing-machine/src/main/java/org/personal/washingmachine/exception/GlobalExceptionHandler.java
@@ -4,19 +4,20 @@ import feign.RetryableException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @Value("${spring.application.name}")
     private String microserviceName;
 
     @ExceptionHandler(CustomException.class)
-    ResponseEntity<Object> handleCustomException(CustomException e) {
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    ErrorResponse handleCustomException(CustomException e) {
         log.error("Exception thrown in microservice: {} with the message: {}.",
                 microserviceName,
                 e.getMessage());
@@ -26,9 +27,7 @@ public class GlobalExceptionHandler {
         // I can use it to distinguish between custom and general exceptions in frontend
 
         //TODO: Maybe i should remove ErrorResponse since the frontend only needs a code.
-        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
-
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorResponse);
+        return new ErrorResponse(e.getErrorCode());
     }
 
 // ************************************************************
@@ -36,18 +35,20 @@ public class GlobalExceptionHandler {
 // ************************************************************
 
     @ExceptionHandler(FeignPropagatedException.class)
-    ResponseEntity<Object> handleFeignPropagatedException(FeignPropagatedException e) {
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    ErrorResponse handleFeignPropagatedException(FeignPropagatedException e) {
         log.error("Feign propagated exception with the message: {}",
                 e.getMessage());
 
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
 
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorResponse);
+        return errorResponse;
     }
 
     // Used to send exceptions about not being able to connect to other backends
     @ExceptionHandler(RetryableException.class)
-    ResponseEntity<Object> handleRetryableException(RetryableException e) {
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    ErrorResponse handleRetryableException(RetryableException e) {
         log.error("Feign connection error: " + e.getMessage());
 
         ErrorResponse errorResponse = new ErrorResponse();
@@ -56,26 +57,24 @@ public class GlobalExceptionHandler {
             errorResponse.setErrorCode(ErrorCode.E_2000);
         }
 
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorResponse);
+        return errorResponse;
     }
 
     @ExceptionHandler(Feign4xxClientException.class)
-    ResponseEntity<Object> handleFeign4xxClientException(Feign4xxClientException e) {
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    ErrorResponse handleFeign4xxClientException(Feign4xxClientException e) {
         log.error("Feign 4xxx Exception : {}",
                 e.getMessage());
 
-        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
-
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorResponse);
+        return new ErrorResponse(e.getErrorCode());
     }
 
     @ExceptionHandler(Feign5xxClientException.class)
-    ResponseEntity<Object> handleFeign5xxClientException(Feign5xxClientException e) {
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    ErrorResponse handleFeign5xxClientException(Feign5xxClientException e) {
         log.error("Feign 5xx Exception : {}",
                 e.getMessage());
 
-        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
-
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(errorResponse);
+        return new ErrorResponse(e.getErrorCode());
     }
 }
