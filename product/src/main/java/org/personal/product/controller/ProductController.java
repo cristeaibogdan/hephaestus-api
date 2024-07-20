ba11b9f762cfb573d7d54279af3ddfd7d35363fc
Bogdan <cristea.ibogdan@gmail.com>
2024-07-20 17:56:29 +0300

    removed placeholder customexception

--- product/src/main/java/org/personal/product/controller/ProductController.java
+++ product/src/main/java/org/personal/product/controller/ProductController.java
@@ -1,10 +1,8 @@
 package org.personal.product.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.personal.product.entity.dtos.ProductModelTypeDTO;
 import org.personal.product.service.ProductService;
-import lombok.RequiredArgsConstructor;
-import org.personal.shared.exception.CustomException;
-import org.personal.shared.exception.ErrorCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,16 +15,15 @@ import java.util.List;
 @RequestMapping("/api/v1/products")
 class ProductController {
 
-    private final ProductService productService;
+	private final ProductService productService;
 
-    @GetMapping("/{category}/manufacturers")
-    List<String> getManufacturers(@PathVariable String category) {
-        throw new CustomException(ErrorCode.NO_MANUFACTURERS_FOUND);
-//        return productService.getManufacturers(category);
-    }
+	@GetMapping("/{category}/manufacturers")
+	List<String> getManufacturers(@PathVariable String category) {
+		return productService.getManufacturers(category);
+	}
 
-    @GetMapping("/{manufacturer}/models-and-types")
-    List<ProductModelTypeDTO> getModelsAndTypes(@PathVariable String manufacturer) {
-        return productService.getModelsAndTypes(manufacturer);
-    }
+	@GetMapping("/{manufacturer}/models-and-types")
+	List<ProductModelTypeDTO> getModelsAndTypes(@PathVariable String manufacturer) {
+		return productService.getModelsAndTypes(manufacturer);
+	}
 }
