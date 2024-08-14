7ecbd10c32ac31b4a693cb940b808dbc8fa3930c
Bogdan <cristea.ibogdan@gmail.com>
2024-08-14 14:44:15 +0300

    added damageLevel enum

---
+++ washing-machine-app/src/main/java/org/personal/washingmachine/facade/calculators/DamageLevel.java
@@ -0,0 +1,22 @@
+package org.personal.washingmachine.facade.calculators;
+
+import lombok.Getter;
+
+@Getter
+public enum DamageLevel {
+	LEVEL_1(1, "REPACKAGE"),
+	LEVEL_2(2, "RESALE"),
+	LEVEL_3(3, "RESALE"),
+	LEVEL_4(4, "REPAIR"),
+	LEVEL_5(5, "DISASSEMBLE");
+
+	private final int level;
+	private final String recommendation;
+
+	DamageLevel(int level, String recommendation) {
+		this.level = level;
+		this.recommendation = recommendation;
+	}
+
+	//TODO: Find a better implementation, or completly remove the need to have level in the DB
+}
