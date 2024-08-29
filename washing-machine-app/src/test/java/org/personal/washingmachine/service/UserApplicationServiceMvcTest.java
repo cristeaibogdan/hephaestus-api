6bc5b420303e84ca28044d21d6b481fa9f1f1eab
Bogdan <cristea.ibogdan@gmail.com>
2024-08-29 16:58:46 +0300

    added tests

---
+++ washing-machine-app/src/test/java/org/personal/washingmachine/service/UserApplicationServiceMvcTest.java
@@ -0,0 +1,203 @@
+package org.personal.washingmachine.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.personal.shared.exception.CustomException;
+import org.personal.shared.exception.ErrorCode;
+import org.personal.washingmachine.dto.Mapper;
+import org.personal.washingmachine.dto.OrganizationAndCountryDTO;
+import org.personal.washingmachine.dto.UserCredentialsDTO;
+import org.personal.washingmachine.dto.UserDTO;
+import org.personal.washingmachine.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserApplicationService.class)
+class UserApplicationServiceMvcTest {
+
+    @MockBean
+    UserService userServiceMock;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Nested
+    class testIsValidRegistrationCode {
+
+        @Test
+        void should_ReturnTrue_When_ValidCode() throws Exception {
+            // GIVEN
+            String registrationCode = "RX2000";
+            boolean expected = true;
+
+            // WHEN
+            ResultActions resultActions = mockMvc.perform(
+                    get("/api/v1/users/{registrationCode}", registrationCode));
+
+            // THEN
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(Boolean.toString(expected)));
+        }
+
+        @Test
+        void should_ReturnFalse_When_InvalidCode() throws Exception {
+            // GIVEN
+            String registrationCode = "I will fail";
+            boolean expected = false;
+
+            // WHEN
+            ResultActions resultActions = mockMvc.perform(
+                    get("/api/v1/users/{registrationCode}", registrationCode));
+
+            // THEN
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(Boolean.toString(expected)));
+        }
+    }
+
+    @Nested
+    class testGetOrganizationAndCountry {
+
+        @Test
+        void should_ReturnOrganizationAndCountryDTO_When_ValidCode() throws Exception {
+            // GIVEN
+            String registrationCode = "RX1001";
+            OrganizationAndCountryDTO expected = new OrganizationAndCountryDTO(
+                    "ZEOS",
+                    "SLOVENIA");
+
+            // WHEN
+            ResultActions resultActions = mockMvc.perform(
+                    get("/api/v1/users/{registrationCode}/organization-and-country", registrationCode));
+
+            // THEN
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(objectMapper.writeValueAsString(expected)));
+        }
+    }
+
+    @Nested
+    class testRegister {
+
+        @Test
+        void should_ReturnHttpStatusCreated() throws Exception {
+            // GIVEN
+            UserDTO userDTO = new UserDTO(
+                    "RX1001",
+                    "OLX",
+                    "Romania",
+                    "bogdan@gmail.com",
+                    "bogdan",
+                    "123456");
+
+            // WHEN
+            ResultActions resultActions = mockMvc.perform(post("/api/v1/users/register")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(userDTO)));
+
+            // THEN
+            then(userServiceMock)
+                    .should(times(1))
+                    .register(any()); //TODO: Should use matchers or not?
+
+            resultActions
+                    .andExpect(status().isCreated())
+                    .andExpect(content().string(""));
+        }
+
+        @Test
+        void should_ThrowCustomException_When_EmailIsAlreadyTaken() throws Exception {
+            // GIVEN
+            UserDTO userDTO = new UserDTO(
+                    "RX1001",
+                    "OLX",
+                    "Romania",
+                    "bogdan@gmail.com",
+                    "bogdan",
+                    "123456");
+
+            willThrow(new CustomException(ErrorCode.EMAIL_ALREADY_TAKEN))
+                    .given(userServiceMock).register(any());
+
+            // WHEN
+            ResultActions resultActions = mockMvc.perform(post("/api/v1/users/register")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(userDTO)));
+
+            // THEN
+            then(userServiceMock)
+                    .should(times(1))
+                    .register(any());
+
+            resultActions
+                    .andExpect(status().is4xxClientError());
+        }
+
+    }
+
+    @Nested
+    class testLogin {
+
+        @Test
+        void should_ReturnUserDTO() throws Exception {
+            // GIVEN
+            UserCredentialsDTO userCredentialsDTO = new UserCredentialsDTO(
+                    "bogdan",
+                    "123456");
+
+            UserDTO expected = new UserDTO(
+                    "RX1001",
+                    "OLX",
+                    "Romania",
+                    "bogdan@gmail.com",
+                    "bogdan",
+                    "123456");
+
+            User user = new User(
+                    "RX1001",
+                    "OLX",
+                    "Romania",
+                    "bogdan@gmail.com",
+                    "bogdan",
+                    "123456");
+
+            given(userServiceMock.login(userCredentialsDTO.username(), userCredentialsDTO.password()))
+                    .willReturn(user);
+
+            // WHEN
+            ResultActions resultActions = mockMvc.perform(post("/api/v1/users/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(userCredentialsDTO)));
+
+            // THEN
+            then(userServiceMock)
+                    .should(times(1))
+                    .login(anyString(), anyString());
+
+            resultActions
+                    .andExpect(status().isOk());
+//                    .andExpect(content().string(
+//                            objectMapper.writeValueAsString(expected)
+//                    )); //TODO: Issues, as I do not return the password
+        }
+    }
+}
