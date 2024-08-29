e8ccf7df56353a325e334091246f33e319febd4e
Bogdan <cristea.ibogdan@gmail.com>
2024-08-29 12:51:46 +0300

    added exception in case all calculations return NONE

--- washing-machine-app/src/main/java/org/personal/washingmachine/service/WashingMachineDamageCalculator.java
+++ washing-machine-app/src/main/java/org/personal/washingmachine/service/WashingMachineDamageCalculator.java
@@ -1,13 +1,20 @@
 package org.personal.washingmachine.service;
 
 import lombok.RequiredArgsConstructor;
+import org.personal.shared.exception.CustomException;
+import org.personal.shared.exception.ErrorCode;
 import org.personal.washingmachine.dto.WashingMachineDetailsDTO;
 import org.personal.washingmachine.enums.Recommendation;
-import org.personal.washingmachine.service.calculators.*;
+import org.personal.washingmachine.service.calculators.HiddenSurfacesDamageCalculator;
+import org.personal.washingmachine.service.calculators.PackageDamageCalculator;
+import org.personal.washingmachine.service.calculators.PricingDamageCalculator;
+import org.personal.washingmachine.service.calculators.VisibleSurfacesDamageCalculator;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.Collections;
+
+import static org.personal.washingmachine.enums.Recommendation.NONE;
 
 @Service
 @RequiredArgsConstructor
@@ -24,11 +31,17 @@ public class WashingMachineDamageCalculator {
 		Recommendation recommendationForHiddenSurfaces = hiddenSurfacesDamageCalculator.calculate(dto);
 		Recommendation recommendationForPricing = pricingDamageCalculator.calculate(dto);
 
-		return Collections.max(Arrays.asList(
+		Recommendation result = Collections.max(Arrays.asList(
 				recommendationForPackage,
 				recommendationForVisibleSurfaces,
 				recommendationForHiddenSurfaces,
 				recommendationForPricing
 		));
+
+		if (result == NONE) {
+			throw new CustomException("Invalid recommendation issued " + NONE, ErrorCode.GENERAL);
+		}
+
+		return result;
 	}
 }
