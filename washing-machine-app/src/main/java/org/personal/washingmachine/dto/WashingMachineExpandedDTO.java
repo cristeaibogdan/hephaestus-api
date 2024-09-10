c62158c38ea56e97b74af03a2b44755a177b6112
Bogdan <cristea.ibogdan@gmail.com>
2024-09-10 20:53:02 +0300

    renamed washingMachineDetails to washingMachineDetail, notice the missin s

--- washing-machine-app/src/main/java/org/personal/washingmachine/dto/WashingMachineExpandedDTO.java
+++ washing-machine-app/src/main/java/org/personal/washingmachine/dto/WashingMachineExpandedDTO.java
@@ -3,6 +3,6 @@ package org.personal.washingmachine.dto;
 import java.util.List;
 
 public record WashingMachineExpandedDTO(
-        WashingMachineDetailDTO washingMachineDetails,
+        WashingMachineDetailDTO washingMachineDetail,
         List<WashingMachineImageDTO> washingMachineImages
 ) {}
