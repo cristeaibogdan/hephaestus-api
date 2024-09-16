ac46311ea62dd11957b1ae7802686fcf13ce7785
Bogdan <cristea.ibogdan@gmail.com>
2024-09-16 18:25:37 +0300

    removed WashingMachineDetail applicable columns from DB

---
+++ washing-machine-app/src/main/resources/db/migration/V2__drop-washing-machine-detail-applicable-columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.washing_machine_detail
+    DROP COLUMN applicable_package_damage,
+    DROP COLUMN applicable_visible_surfaces_damage,
+    DROP COLUMN applicable_hidden_surfaces_damage;
