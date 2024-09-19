df3e16819c19248b9fb33a8ae4eed5c1f7f6f470
Bogdan <cristea.ibogdan@gmail.com>
2024-09-19 16:01:09 +0300

    removed unused applicable visible surfaces columns from DB

---
+++ washing-machine-app/src/main/resources/db/migration/V4__drop-washing-machine-detail-visible-applicable-columns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.washing_machine_detail
+    DROP COLUMN visible_surfaces_has_scratches,
+    DROP COLUMN visible_surfaces_has_dents,
+    DROP COLUMN visible_surfaces_has_minor_damage,
+    DROP COLUMN visible_surfaces_has_major_damage;
