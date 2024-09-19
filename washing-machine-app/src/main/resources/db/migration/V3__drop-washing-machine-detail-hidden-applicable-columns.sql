97218406f70bfd516b68f97e03d3216f3e85771f
Bogdan <cristea.ibogdan@gmail.com>
2024-09-19 15:17:54 +0300

    dropped unused hiddensurfaces columns from the db

---
+++ washing-machine-app/src/main/resources/db/migration/V3__drop-washing-machine-detail-hidden-applicable-columns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.washing_machine_detail
+    DROP COLUMN hidden_surfaces_has_scratches,
+    DROP COLUMN hidden_surfaces_has_dents,
+    DROP COLUMN hidden_surfaces_has_minor_damage,
+    DROP COLUMN hidden_surfaces_has_major_damage;
