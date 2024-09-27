674610ca2cc7e7672ebca28a24ff0b98aacc8788
Bogdan <cristea.ibogdan@gmail.com>
2024-09-27 09:36:40 +0300

    Replaced manual builder with lombok's builder

--- washing-machine-domain/src/main/java/org/personal/washingmachine/entity/embedded/HiddenSurfaceDamage.java
+++ washing-machine-domain/src/main/java/org/personal/washingmachine/entity/embedded/HiddenSurfaceDamage.java
@@ -2,15 +2,12 @@ package org.personal.washingmachine.entity.embedded;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
+import lombok.*;
 
-import static org.apache.commons.lang3.StringUtils.*;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
@@ -49,46 +46,5 @@ public class HiddenSurfaceDamage {
 
 	public boolean hasMajorDamage() {
 		return isNotBlank(hiddenSurfacesMajorDamage);
-	}
-
-	public static HiddenSurfaceDamageBuilder builder() { //TODO: Replace with lombok @Builder
-		return new HiddenSurfaceDamageBuilder();
-	}
-
-	public static class HiddenSurfaceDamageBuilder {
-		private double hiddenSurfacesScratchesLength;
-		private double hiddenSurfacesDentsDepth;
-
-		private String hiddenSurfacesMinorDamage;
-		private String hiddenSurfacesMajorDamage;
-
-		public HiddenSurfaceDamage build() {
-			return new HiddenSurfaceDamage(
-					this.hiddenSurfacesScratchesLength,
-					this.hiddenSurfacesDentsDepth,
-					this.hiddenSurfacesMinorDamage,
-					this.hiddenSurfacesMajorDamage
-			);
-		}
-
-        public HiddenSurfaceDamageBuilder hiddenSurfacesScratchesLength (double hiddenSurfacesScratchesLength) {
-            this.hiddenSurfacesScratchesLength = hiddenSurfacesScratchesLength;
-            return this;
-        }
-
-        public HiddenSurfaceDamageBuilder hiddenSurfacesDentsDepth (double hiddenSurfacesDentsDepth) {
-            this.hiddenSurfacesDentsDepth = hiddenSurfacesDentsDepth;
-            return this;
-        }
-
-        public HiddenSurfaceDamageBuilder hiddenSurfacesMinorDamage (String hiddenSurfacesMinorDamage) {
-            this.hiddenSurfacesMinorDamage = hiddenSurfacesMinorDamage;
-            return this;
-        }
-
-        public HiddenSurfaceDamageBuilder hiddenSurfacesMajorDamage (String hiddenSurfacesMajorDamage) {
-            this.hiddenSurfacesMajorDamage = hiddenSurfacesMajorDamage;
-            return this;
-        }
 	}
 }
--- washing-machine-domain/src/main/java/org/personal/washingmachine/entity/embedded/VisibleSurfaceDamage.java
+++ washing-machine-domain/src/main/java/org/personal/washingmachine/entity/embedded/VisibleSurfaceDamage.java
@@ -2,92 +2,49 @@ package org.personal.washingmachine.entity.embedded;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class VisibleSurfaceDamage {
 
-    @Column(name = "visible_surfaces_scratches_length")
-    private double visibleSurfacesScratchesLength;
+	@Column(name = "visible_surfaces_scratches_length")
+	private double visibleSurfacesScratchesLength;
 
-    @Column(name = "visible_surfaces_dents_depth")
-    private double visibleSurfacesDentsDepth;
+	@Column(name = "visible_surfaces_dents_depth")
+	private double visibleSurfacesDentsDepth;
 
-    @Column(name = "visible_surfaces_minor_damage")
-    private String visibleSurfacesMinorDamage;
+	@Column(name = "visible_surfaces_minor_damage")
+	private String visibleSurfacesMinorDamage;
 
-    @Column(name = "visible_surfaces_major_damage")
-    private String visibleSurfacesMajorDamage;
+	@Column(name = "visible_surfaces_major_damage")
+	private String visibleSurfacesMajorDamage;
 
-    public boolean isApplicable() {
-        return hasScratches() ||
-                hasDents() ||
-                hasMinorDamage() ||
-                hasMajorDamage();
-    }
+	public boolean isApplicable() {
+		return hasScratches() ||
+				hasDents() ||
+				hasMinorDamage() ||
+				hasMajorDamage();
+	}
 
-    public boolean hasScratches() {
-        return visibleSurfacesScratchesLength > 0;
-    }
+	public boolean hasScratches() {
+		return visibleSurfacesScratchesLength > 0;
+	}
 
-    public boolean hasDents() {
-        return visibleSurfacesDentsDepth > 0;
-    }
+	public boolean hasDents() {
+		return visibleSurfacesDentsDepth > 0;
+	}
 
-    public boolean hasMinorDamage() {
-        return isNotBlank(visibleSurfacesMinorDamage);
-    }
+	public boolean hasMinorDamage() {
+		return isNotBlank(visibleSurfacesMinorDamage);
+	}
 
-    public boolean hasMajorDamage() {
-        return isNotBlank(visibleSurfacesMajorDamage);
-    }
-
-    public static VisibleSurfaceDamageBuilder builder() { //TODO: Replace with lombok @Builder
-        return new VisibleSurfaceDamageBuilder();
-    }
-
-    public static class VisibleSurfaceDamageBuilder {
-        private double visibleSurfacesScratchesLength;
-        private double visibleSurfacesDentsDepth;
-
-        private String visibleSurfacesMinorDamage;
-        private String visibleSurfacesMajorDamage;
-
-        public VisibleSurfaceDamage build() {
-            return new VisibleSurfaceDamage(
-                    this.visibleSurfacesScratchesLength,
-                    this.visibleSurfacesDentsDepth,
-                    this.visibleSurfacesMinorDamage,
-                    this.visibleSurfacesMajorDamage
-            );
-        }
-
-        public VisibleSurfaceDamageBuilder visibleSurfacesScratchesLength(double visibleSurfacesScratchesLength) {
-            this.visibleSurfacesScratchesLength = visibleSurfacesScratchesLength;
-            return this;
-        }
-
-        public VisibleSurfaceDamageBuilder visibleSurfacesDentsDepth (double visibleSurfacesDentsDepth) {
-            this.visibleSurfacesDentsDepth = visibleSurfacesDentsDepth;
-            return this;
-        }
-
-        public VisibleSurfaceDamageBuilder visibleSurfacesMinorDamage (String visibleSurfacesMinorDamage) {
-            this.visibleSurfacesMinorDamage = visibleSurfacesMinorDamage;
-            return this;
-        }
-
-        public VisibleSurfaceDamageBuilder visibleSurfacesMajorDamage (String visibleSurfacesMajorDamage) {
-            this.visibleSurfacesMajorDamage = visibleSurfacesMajorDamage;
-            return this;
-        }
-    }
+	public boolean hasMajorDamage() {
+		return isNotBlank(visibleSurfacesMajorDamage);
+	}
 }
