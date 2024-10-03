b8e7e6c728a0e65c78d54fbd06804a4bbce4c92c
Bogdan <cristea.ibogdan@gmail.com>
2024-10-03 19:14:54 +0300

    modified test to check for missing translation keys

--- washing-machine-app/src/test/java/org/personal/washingmachine/service/DynamicFilteringExceptionTest.java
+++ washing-machine-app/src/test/java/org/personal/washingmachine/service/DynamicFilteringExceptionTest.java
@@ -2,6 +2,9 @@ package org.personal.washingmachine.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.personal.shared.clients.ProductClient;
 import org.personal.washingmachine.dto.PageRequestDTO;
 import org.personal.washingmachine.repository.WashingMachineRepository;
@@ -12,8 +15,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.notNullValue;
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -82,8 +86,15 @@ class DynamicFilteringExceptionTest {
 				.andExpect(content().string(containsString("pageSize")));
 	}
 
-	@Test
-	void should_ThrowCustomException_When_InvalidDate() throws Exception {
+	private static final String[] availableLocales = {"en", "es", "it", "ro", "sl"};
+
+	private static Stream<String> localeProvider() {
+		return Stream.of(availableLocales);
+	}
+
+	@ParameterizedTest
+	@MethodSource("localeProvider")
+	void should_ThrowCustomException_When_InvalidDate(String locale) throws Exception {
 		// GIVEN
 		PageRequestDTO dto = defaultPageRequest.toBuilder()
 				.createdAt("invalid date")
@@ -93,11 +104,12 @@ class DynamicFilteringExceptionTest {
 		ResultActions resultActions = mockMvc.perform(
 				post("/api/v1/washing-machines")
 						.content(jackson.writeValueAsString(dto))
-						.contentType(MediaType.APPLICATION_JSON));
+						.contentType(MediaType.APPLICATION_JSON)
+						.header("Accept-Language", locale));
 
 		// THEN
 		resultActions
 				.andExpect(status().is4xxClientError())
-				.andExpect(content().string(notNullValue()));
+				.andExpect(content().string(not(containsString("Internal Translation Error"))));
 	}
 }
