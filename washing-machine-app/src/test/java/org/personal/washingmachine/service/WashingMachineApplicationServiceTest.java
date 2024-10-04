6b9f99edfc9aa5ae639cc405c3785168ebc7f10b
Bogdan <cristea.ibogdan@gmail.com>
2024-10-04 14:48:22 +0300

    removed overlapping test

--- washing-machine-app/src/test/java/org/personal/washingmachine/service/WashingMachineApplicationServiceTest.java
+++ washing-machine-app/src/test/java/org/personal/washingmachine/service/WashingMachineApplicationServiceTest.java
@@ -1,18 +1,10 @@
 package org.personal.washingmachine.service;
 
-import com.querydsl.core.types.Predicate;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.personal.shared.clients.ProductClient;
-import org.personal.washingmachine.WashingMachineTestData;
-import org.personal.washingmachine.dto.PageRequestDTO;
-import org.personal.washingmachine.dto.WashingMachineDetailDTO;
-import org.personal.washingmachine.dto.WashingMachineSimpleDTO;
 import org.personal.washingmachine.entity.WashingMachine;
 import org.personal.washingmachine.entity.WashingMachineDetail;
 import org.personal.washingmachine.entity.embedded.HiddenSurfaceDamage;
@@ -27,136 +19,106 @@ import org.personal.washingmachine.service.calculators.HiddenSurfacesRecommendat
 import org.personal.washingmachine.service.calculators.PackageRecommendationCalculator;
 import org.personal.washingmachine.service.calculators.PricingRecommendationCalculator;
 import org.personal.washingmachine.service.calculators.VisibleSurfacesRecommendationCalculator;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.personal.washingmachine.enums.Recommendation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 class WashingMachineApplicationServiceTest {
-    WashingMachineRepository washingMachineRepositoryMock = mock(WashingMachineRepository.class);
+	WashingMachineRepository washingMachineRepositoryMock = mock(WashingMachineRepository.class);
 
-    WashingMachineService washingMachineService = new WashingMachineService(washingMachineRepositoryMock);
-    WashingMachineDamageCalculator washingMachineDamageCalculator = new WashingMachineDamageCalculator(
-            new PackageRecommendationCalculator(),
-            new VisibleSurfacesRecommendationCalculator(),
-            new HiddenSurfacesRecommendationCalculator(),
-            new PricingRecommendationCalculator()
-    );
-    WashingMachineReportGenerator washingMachineReportGenerator = new WashingMachineReportGenerator();
+	WashingMachineService washingMachineService = new WashingMachineService(washingMachineRepositoryMock);
+	WashingMachineDamageCalculator washingMachineDamageCalculator = new WashingMachineDamageCalculator(
+			new PackageRecommendationCalculator(),
+			new VisibleSurfacesRecommendationCalculator(),
+			new HiddenSurfacesRecommendationCalculator(),
+			new PricingRecommendationCalculator()
+	);
+	WashingMachineReportGenerator washingMachineReportGenerator = new WashingMachineReportGenerator();
 
-    ProductClient productClient; //TODO: To be deleted
+	ProductClient productClient; //TODO: To be deleted
 
-    WashingMachineApplicationService underTest = new WashingMachineApplicationService(
-            washingMachineService,
-            washingMachineRepositoryMock,
-            washingMachineDamageCalculator,
-            washingMachineReportGenerator,
-            productClient
-    );
+	WashingMachineApplicationService underTest = new WashingMachineApplicationService(
+			washingMachineService,
+			washingMachineRepositoryMock,
+			washingMachineDamageCalculator,
+			washingMachineReportGenerator,
+			productClient
+	);
 
-    @Nested
-    class testGetRecommendation {
+	@Nested
+	class testGetReport {
 
-        @Test
-        void should_ReturnRESALE_When_PackageDirtyTrue() {
-            // GIVEN
-            WashingMachineDetailDTO dto = WashingMachineDetailDTO.builder()
-                    .applicablePackageDamage(true)
-                    .packageDirty(true)
-                    .build();
+		@Test
+		void should_ReturnReport() {
+			// GIVEN
+			String serialNumber = "ABC-987";
 
-            Recommendation expected = RESALE;
+			WashingMachineDetail washingMachineDetail = new WashingMachineDetail(
+					new PackageDamage(true, true, true),
+					new VisibleSurfaceDamage(
+							5.5,
+							0,
+							"some minor damage",
+							"some major damage"
+					),
+					new HiddenSurfaceDamage(
+							5.5,
+							0,
+							"some minor damage",
+							"some major damage"
+					),
+					0,
+					0
+			);
 
-            // WHEN
-            Recommendation actual = underTest.getRecommendation(dto);
+			WashingMachine washingMachine = new WashingMachine(
+					"Washing Machine",
+					"Whirlpool",
+					DamageType.IN_USE,
+					ReturnType.SERVICE,
+					IdentificationMode.DATA_MATRIX,
+					"test",
+					"modelOne",
+					"typeOne",
+					Recommendation.RESALE,
+					washingMachineDetail
+			);
 
-            // THEN
-            assertThat(actual).isEqualTo(expected);
-        }
-    }
+			// washingMachine.addImage(); TODO: How to create images?
 
-    @Nested
-    class testGetReport {
+			given(washingMachineRepositoryMock.findBySerialNumber(serialNumber))
+					.willReturn(Optional.of(washingMachine));
 
-        @Test
-        void should_ReturnReport() {
-            // GIVEN
-            String serialNumber = "ABC-987";
-
-            WashingMachineDetail washingMachineDetail = new WashingMachineDetail(
-                    new PackageDamage(true, true, true),
-                    new VisibleSurfaceDamage(
-                            5.5,
-                            0,
-                            "some minor damage",
-                            "some major damage"
-                    ),
-                    new HiddenSurfaceDamage(
-                            5.5,
-                            0,
-                            "some minor damage",
-                            "some major damage"
-                    ),
-                    0,
-                    0
-            );
-
-            WashingMachine washingMachine = new WashingMachine(
-                    "Washing Machine",
-                    "Whirlpool",
-                    DamageType.IN_USE,
-                    ReturnType.SERVICE,
-                    IdentificationMode.DATA_MATRIX,
-                    "test",
-                    "modelOne",
-                    "typeOne",
-                    Recommendation.RESALE,
-                    washingMachineDetail
-            );
-
-            // washingMachine.addImage(); TODO: How to create images?
-
-            given(washingMachineRepositoryMock.findBySerialNumber(serialNumber))
-                    .willReturn(Optional.of(washingMachine));
-
-            // WHEN
+			// WHEN
 //            WashingMachineReportDTO report = underTest.getReport(serialNumber);
 
-            // THEN
+			// THEN
 //            assertThat(report.createdAt())
 //                    .isEqualTo(LocalDateTime.now()); TODO: This is not possible, as hibernate generated the date after saving...
-        }
-    }
+		}
+	}
 
-    @Nested
-    class testIsSerialNumberInUse {
+	@Nested
+	class testIsSerialNumberInUse {
 
-        @ParameterizedTest
-        @ValueSource(booleans = {true, false})
-        void should_ReturnTrue(boolean expected) {
-            // GIVEN
-            String serialNumber = "ABC-987";
+		@ParameterizedTest
+		@ValueSource(booleans = {true, false})
+		void should_ReturnTrue(boolean expected) {
+			// GIVEN
+			String serialNumber = "ABC-987";
 
-            given(washingMachineRepositoryMock.existsBySerialNumber(serialNumber))
-                    .willReturn(expected);
+			given(washingMachineRepositoryMock.existsBySerialNumber(serialNumber))
+					.willReturn(expected);
 
-            // WHEN
-            boolean actual = underTest.isSerialNumberInUse(serialNumber);
+			// WHEN
+			boolean actual = underTest.isSerialNumberInUse(serialNumber);
 
-            // THEN
-            assertThat(actual).isEqualTo(expected);
-        }
-    }
+			// THEN
+			assertThat(actual).isEqualTo(expected);
+		}
+	}
 
 }
