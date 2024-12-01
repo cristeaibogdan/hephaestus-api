d679f5d7914a1d6cbbb97f587fcf36ef49cffe5f
Bogdan <cristea.ibogdan@gmail.com>
2024-12-01 15:35:45 +0200

    Replaced @NotBlank with @NotNull in cases where the property can be an empty string

--- washing-machine-app/src/main/java/org/personal/washingmachine/dto/CreateWashingMachineDetailRequest.java
+++ washing-machine-app/src/main/java/org/personal/washingmachine/dto/CreateWashingMachineDetailRequest.java
@@ -20,11 +20,11 @@ public record CreateWashingMachineDetailRequest(
         double visibleSurfacesDentsDepth,
 
 		@Size(max = 200, message = "{STRING_MAXIMUM_LENGTH}")
-		@NotBlank(message = "{FIELD_NOT_BLANK}")
+		@NotNull(message = "{FIELD_NOT_NULL}")
         String visibleSurfacesMinorDamage,
 
 		@Size(max = 200, message = "{STRING_MAXIMUM_LENGTH}")
-		@NotBlank(message = "{FIELD_NOT_BLANK}")
+		@NotNull(message = "{FIELD_NOT_NULL}")
 		String visibleSurfacesMajorDamage,
 
         // HIDDEN SURFACES
@@ -37,11 +37,11 @@ public record CreateWashingMachineDetailRequest(
         double hiddenSurfacesDentsDepth,
 
 		@Size(max = 200, message = "{STRING_MAXIMUM_LENGTH}")
-		@NotBlank(message = "{FIELD_NOT_BLANK}")
+		@NotNull(message = "{FIELD_NOT_NULL}")
         String hiddenSurfacesMinorDamage,
 
 		@Size(max = 200, message = "{STRING_MAXIMUM_LENGTH}")
-		@NotBlank(message = "{FIELD_NOT_BLANK}")
+		@NotNull(message = "{FIELD_NOT_NULL}")
         String hiddenSurfacesMajorDamage,
 
 		// PRICING
