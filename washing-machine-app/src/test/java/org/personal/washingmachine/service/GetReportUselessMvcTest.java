4b46d3ea59f1c50c5892ef5461ee7f083b98c8b1
Bogdan <cristea.ibogdan@gmail.com>
2024-12-09 16:41:39 +0200

    Added deprecated

--- washing-machine-app/src/test/java/org/personal/washingmachine/service/GetReportUselessMvcTest.java
+++ washing-machine-app/src/test/java/org/personal/washingmachine/service/GetReportUselessMvcTest.java
@@ -18,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Deprecated
 @WebMvcTest(WashingMachineApplicationService.class)
 class GetReportUselessMvcTest {
 
