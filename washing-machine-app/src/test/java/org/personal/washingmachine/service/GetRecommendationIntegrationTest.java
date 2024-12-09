c9a0f0b4e0162918ea123791fea1752a10c55081
Bogdan <cristea.ibogdan@gmail.com>
2024-12-09 16:54:05 +0200

    Refactored Mvc test to integration test.

---
+++ washing-machine-app/src/test/java/org/personal/washingmachine/service/GetRecommendationIntegrationTest.java
@@ -0,0 +1,92 @@
+package org.personal.washingmachine.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
+import org.personal.washingmachine.BaseIntegrationTest;
+import org.personal.washingmachine.entity.WashingMachine;
+import org.personal.washingmachine.enums.DamageType;
+import org.personal.washingmachine.enums.IdentificationMode;
+import org.personal.washingmachine.enums.Recommendation;
+import org.personal.washingmachine.enums.ReturnType;
+import org.personal.washingmachine.repository.WashingMachineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GetRecommendationIntegrationTest extends BaseIntegrationTest {
+
+	@Autowired MockMvc mockMvc;
+	@Autowired ObjectMapper jackson;
+
+	@Autowired WashingMachineApplicationService underTest;
+	@Autowired WashingMachineRepository washingMachineRepository;
+
+	@BeforeAll
+	void loadDataInDB() {
+		washingMachineRepository.save(
+				new WashingMachine(
+						"Washing Machine",
+						"Gorenje",
+						DamageType.IN_USE,
+						ReturnType.COMMERCIAL,
+						IdentificationMode.DATA_MATRIX,
+						"I exist in DB",
+						"modelA",
+						"TypeZ",
+						Recommendation.OUTLET,
+						null
+				)
+		);
+	}
+
+	@AfterAll
+	void cleanUpDB() {
+		washingMachineRepository.deleteAll();
+	}
+
+	@Nested
+	class MvcTest {
+
+		@Test
+		void should_ReturnStatusOk() throws Exception {
+			// GIVEN
+			String serialNumber = "I exist in DB";
+
+			// WHEN
+			ResultActions resultActions = performRequest(serialNumber);
+
+			// THEN
+			resultActions.andExpect(status().isOk());
+		}
+
+		@Test
+		void should_ThrowCustomException_When_SerialNumberNotFound() throws Exception {
+			// GIVEN
+			String serialNumber = "I don't exist";
+
+			// WHEN
+			ResultActions resultActions = performRequest(serialNumber);
+
+			// THEN
+			resultActions
+					.andExpect(status().isNotFound())
+					.andExpect(content().string(not(containsString("Internal Translation Error"))));
+		}
+
+		private ResultActions performRequest(String serialNumber) throws Exception {
+			return mockMvc.perform(
+					get("/api/v1/washing-machines/{serialNumber}/recommendation", serialNumber)
+			);
+		}
+	}
+
+}
