627c5a05617a6b00941011dcad201ada2dfcec34
Bogdan <cristea.ibogdan@gmail.com>
2024-12-16 17:27:25 +0200

    added comments about @Transactional in BaseIntegrationTest

--- washing-machine-app/src/test/java/org/personal/washingmachine/BaseIntegrationTest.java
+++ washing-machine-app/src/test/java/org/personal/washingmachine/BaseIntegrationTest.java
@@ -6,6 +6,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+/*
+	@Transactional
+	on class = is applied on all methods; every @Test + @BeforeEach and anything before
+ 	Executes in a transaction. When you finish a @Test, it ROLLSBACK, it DOES NOT COMMIT to DB.
+
+ 	Clean and automated, but you may miss:
+ 		- Unique at insert/update
+ 		- FK violations
+ 		- not null
+ 		- other DB triggers
+
+ 	Limitations: Can't test code that uses:
+ 		- @Async
+ 		- @Transactional(REQUIRES_NEW)
+*/
+
 @Tag("slow")
 @AutoConfigureMockMvc // avoids swapping threads in Tomcat's tread pool so @Transactional still works
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
