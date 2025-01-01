baceb5614294ef4d83bf95cdb91036324234e345
Bogdan <cristea.ibogdan@gmail.com>
2025-01-01 14:41:15 +0200

    remove test data dependency

--- washing-machine-app/src/test/java/org/personal/washingmachine/service/LoadPaginatedAndFilteredValidationMvcTest.java
+++ washing-machine-app/src/test/java/org/personal/washingmachine/service/LoadPaginatedAndFilteredValidationMvcTest.java
@@ -3,7 +3,6 @@ package org.personal.washingmachine.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.personal.shared.clients.ProductClient;
-import org.personal.washingmachine.TestData;
 import org.personal.washingmachine.dto.SearchWashingMachineRequest;
 import org.personal.washingmachine.repository.WashingMachineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,15 +29,29 @@ class LoadPaginatedAndFilteredValidationMvcTest {
 	@MockBean WashingMachineReportGenerator reportGenerator;
 	@MockBean ProductClient productClient; //TODO: To be deleted
 
+	SearchWashingMachineRequest searchWashingMachineRequest = new SearchWashingMachineRequest(
+			0,
+			2,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null
+	);
+
 	@Test
 	void should_ThrowValidationException_When_PageIndexIsNegative() throws Exception {
 		// GIVEN
-		SearchWashingMachineRequest request = TestData.searchWashingMachineRequest().toBuilder()
-				.pageIndex(-1)
-				.build();
-
 		// WHEN
-		ResultActions resultActions = performRequest(request);
+		ResultActions resultActions = performRequest(
+				searchWashingMachineRequest.toBuilder()
+						.pageIndex(-1)
+						.build()
+		);
 
 		// THEN
 		resultActions
@@ -50,12 +63,12 @@ class LoadPaginatedAndFilteredValidationMvcTest {
 	@Test
 	void should_ThrowValidationException_When_PageSizeIsLessThanOne() throws Exception {
 		// GIVEN
-		SearchWashingMachineRequest request = TestData.searchWashingMachineRequest().toBuilder()
-				.pageSize(0)
-				.build();
-
 		// WHEN
-		ResultActions resultActions = performRequest(request);
+		ResultActions resultActions = performRequest(
+				searchWashingMachineRequest.toBuilder()
+						.pageSize(0)
+						.build()
+		);
 
 		// THEN
 		resultActions
