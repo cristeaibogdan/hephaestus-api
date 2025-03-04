2c197a07459186e4abf769b328eed102d223e387
Bogdan <cristea.ibogdan@gmail.com>
2025-03-04 16:48:29 +0200

    Added check for emptyString to ensure that the save method's return type isn't changed

--- solar-panel-app/src/test/java/org/personal/solarpanel/service/SaveIntegrationTest.java
+++ solar-panel-app/src/test/java/org/personal/solarpanel/service/SaveIntegrationTest.java
@@ -129,8 +129,8 @@ class SaveIntegrationTest extends BaseIntegrationTest {
 			);
 
 			// THEN
-			resultActions.andExpect(status().isCreated());
-//					.andExpect(content().string(emptyString()));
+			resultActions.andExpect(status().isCreated())
+					.andExpect(content().string(emptyString()));
 		}
 	}
 
