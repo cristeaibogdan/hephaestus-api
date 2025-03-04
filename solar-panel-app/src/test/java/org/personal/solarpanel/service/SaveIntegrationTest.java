80a2114102aba4070a9518f179059cc27771cb2a
Bogdan <cristea.ibogdan@gmail.com>
2025-03-04 15:48:22 +0200

    Moved tests inside nested IntegrationTest class

--- solar-panel-app/src/test/java/org/personal/solarpanel/service/SaveIntegrationTest.java
+++ solar-panel-app/src/test/java/org/personal/solarpanel/service/SaveIntegrationTest.java
@@ -1,5 +1,6 @@
 package org.personal.solarpanel.service;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.personal.solarpanel.BaseIntegrationTest;
 import org.personal.solarpanel.dto.SaveSolarPanelDamageRequest;
@@ -17,70 +18,74 @@ class SaveIntegrationTest extends BaseIntegrationTest {
 	@Autowired SolarPanelApplicationService underTest;
 	@Autowired SolarPanelRepository repository;
 
-	@Test
-	void should_SaveRequest_With_AllPropertiesInDB() {
-		// GIVEN
-		SaveSolarPanelRequest request = new SaveSolarPanelRequest(
-				"Solar Panel",
-				"Tesla",
-				"model1",
-				"type2",
-				"AX-098-200",
-				new SaveSolarPanelDamageRequest(
-						true,
-						false,
-						true,
-						false,
-						"blue paint was thrown on half the panel"
-				)
-		);
+	@Nested
+	class IntegrationTest {
+		@Test
+		void should_SaveRequest_With_AllPropertiesInDB() {
+			// GIVEN
+			SaveSolarPanelRequest request = new SaveSolarPanelRequest(
+					"Solar Panel",
+					"Tesla",
+					"model1",
+					"type2",
+					"AX-098-200",
+					new SaveSolarPanelDamageRequest(
+							true,
+							false,
+							true,
+							false,
+							"blue paint was thrown on half the panel"
+					)
+			);
 
-		// WHEN
-		underTest.save(request);
-		SolarPanel actual = repository.findBySerialNumber("AX-098-200").orElseThrow();
+			// WHEN
+			underTest.save(request);
+			SolarPanel actual = repository.findBySerialNumber("AX-098-200").orElseThrow();
 
-		// THEN
-		assertThat(actual).satisfies(act -> {
-			// main fields
-			assertThat(act.getCategory()).isEqualTo(request.category());
-			assertThat(act.getManufacturer()).isEqualTo(request.manufacturer());
-			assertThat(act.getModel()).isEqualTo(request.model());
-			assertThat(act.getType()).isEqualTo(request.type());
-			assertThat(act.getSerialNumber()).isEqualTo(request.serialNumber());
+			// THEN
+			assertThat(actual).satisfies(act -> {
+				// main fields
+				assertThat(act.getCategory()).isEqualTo(request.category());
+				assertThat(act.getManufacturer()).isEqualTo(request.manufacturer());
+				assertThat(act.getModel()).isEqualTo(request.model());
+				assertThat(act.getType()).isEqualTo(request.type());
+				assertThat(act.getSerialNumber()).isEqualTo(request.serialNumber());
 
-			// damage
-			assertThat(act.getDamage().isHotSpots()).isEqualTo(request.saveSolarPanelDamageRequest().hotSpots());
-			assertThat(act.getDamage().isMicroCracks()).isEqualTo(request.saveSolarPanelDamageRequest().microCracks());
-			assertThat(act.getDamage().isSnailTrails()).isEqualTo(request.saveSolarPanelDamageRequest().snailTrails());
-			assertThat(act.getDamage().isBrokenGlass()).isEqualTo(request.saveSolarPanelDamageRequest().brokenGlass());
-			assertThat(act.getDamage().getAdditionalDetails()).isEqualTo(request.saveSolarPanelDamageRequest().additionalDetails());
-		});
+				// damage
+				assertThat(act.getDamage().isHotSpots()).isEqualTo(request.saveSolarPanelDamageRequest().hotSpots());
+				assertThat(act.getDamage().isMicroCracks()).isEqualTo(request.saveSolarPanelDamageRequest().microCracks());
+				assertThat(act.getDamage().isSnailTrails()).isEqualTo(request.saveSolarPanelDamageRequest().snailTrails());
+				assertThat(act.getDamage().isBrokenGlass()).isEqualTo(request.saveSolarPanelDamageRequest().brokenGlass());
+				assertThat(act.getDamage().getAdditionalDetails()).isEqualTo(request.saveSolarPanelDamageRequest().additionalDetails());
+			});
+		}
+
+		@Test
+		void should_ReturnCurrentDate_For_createdAtField() {
+			// GIVEN
+			SaveSolarPanelRequest request = new SaveSolarPanelRequest(
+					"Solar Panel",
+					"Tesla",
+					"model1",
+					"type2",
+					"date-test",
+					new SaveSolarPanelDamageRequest(
+							true,
+							false,
+							true,
+							false,
+							"blue paint was thrown on half the panel"
+					)
+			);
+
+			// WHEN
+			underTest.save(request);
+			SolarPanel actual = repository.findBySerialNumber("date-test").orElseThrow();
+
+			// THEN
+			assertThat(actual.getCreatedAt().toLocalDate())
+					.isEqualTo(LocalDate.now());
+		}
 	}
 
-	@Test
-	void should_ReturnCurrentDate_For_createdAtField() {
-		// GIVEN
-		SaveSolarPanelRequest request = new SaveSolarPanelRequest(
-				"Solar Panel",
-				"Tesla",
-				"model1",
-				"type2",
-				"date-test",
-				new SaveSolarPanelDamageRequest(
-						true,
-						false,
-						true,
-						false,
-						"blue paint was thrown on half the panel"
-				)
-		);
-
-		// WHEN
-		underTest.save(request);
-		SolarPanel actual = repository.findBySerialNumber("date-test").orElseThrow();
-
-		// THEN
-		assertThat(actual.getCreatedAt().toLocalDate())
-				.isEqualTo(LocalDate.now());
-	}
 }
