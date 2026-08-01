704e7318da5933c2171d61d04d8f3d753961ed98
Bogdan <cristea.ibogdan@gmail.com>
2026-08-01 19:31:33 +0300

    Remove interface. Inline endpoint uri. Add pathvariable

--- washing-machine-app/src/main/java/org/personal/washingmachine/service/UserApplicationService.java
+++ washing-machine-app/src/main/java/org/personal/washingmachine/service/UserApplicationService.java
@@ -6,34 +6,36 @@ import org.personal.washingmachine.dto.GetOrganizationAndCountryResponse;
 import org.personal.washingmachine.dto.LoginUserRequest;
 import org.personal.washingmachine.dto.LoginUserResponse;
 import org.personal.washingmachine.entity.User;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import org.personal.washingmachine.mapper.UserMapper;
 
 @RestController
 @RequiredArgsConstructor
-class UserApplicationService implements IUserApplicationService { // TODO: Replace with proper authentication
+class UserApplicationService { // TODO: Replace with proper authentication
 	private final UserService service;
 	private final UserMapper userMapper;
 	private final RegistrationCodeContainer registrationCodeContainer = RegistrationCodeContainer.getInstance();
 
-	@Override
-	public boolean isValidRegistrationCode(String registrationCode) {
+	@GetMapping("/api/v1/users/{registrationCode}")
+	public boolean isValidRegistrationCode(@PathVariable String registrationCode) {
 		return registrationCodeContainer.exists(registrationCode);
 	}
 
-	@Override
-	public GetOrganizationAndCountryResponse getOrganizationAndCountry(String registrationCode) {
+	@GetMapping("/api/v1/users/{registrationCode}/organization-and-country")
+	public GetOrganizationAndCountryResponse getOrganizationAndCountry(@PathVariable String registrationCode) {
 		return registrationCodeContainer.getOrganizationAndCountry(registrationCode);
 	}
 
-	@Override
+	@PostMapping("/api/v1/users/register")
+	@ResponseStatus(HttpStatus.CREATED)
 	public void register(CreateUserRequest createUserRequest) {
 		User user = userMapper.toEntity(createUserRequest);
 		service.register(user);
 	}
 
-	@Override
+	@PostMapping("/api/v1/users/login")
 	public LoginUserResponse login(LoginUserRequest loginUserRequest) {
 		User user = service.login(loginUserRequest.username(), loginUserRequest.password());
 		return userMapper.toLoginUserResponse(user);
